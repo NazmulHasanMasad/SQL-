d7f451aa21ff4f1d815af2ace9cb8ffec55875cd
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2026-04-05 22:35:59 +0200

    4_NULL_cs50.sql

---
+++ 4_NULL_cs50.sql
@@ -0,0 +1,9 @@
+-- Books without a translator
+SELECT "title", "translator"
+FROM "longlist"
+WHERE "translator" IS NULL;
+
+-- Books with a translator
+SELECT "title", "translator"
+FROM "longlist"
+WHERE "translator" IS NOT NULL;
